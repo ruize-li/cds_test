eecf74892547b5e2e939e33e48ed04ee2a11cbd9
Ruize Li <ruize.li@icloud.com>
2021-08-05 15:11:35 -0400

    Added turn JS documentation and examples

--- cds_test_frontend/src/components/pages/Homepage.js
+++ cds_test_frontend/src/components/pages/Homepage.js
@@ -7,10 +7,14 @@
  import Turn from "../Turn";
  import './turn.js';
 
+
+// @todo: 
+//         use thumbnails for magazine-like display
+//         on click event: switch to load original / high res page
 const DemoMagazine = () => {
     const options = {
-        width: 800,
-        height: 600,
+        width: 1600,
+        height: 1200,
         autoCenter: true,
         display: "double",
         acceleration: true,
@@ -24,14 +28,14 @@ const DemoMagazine = () => {
       };
       
       const pages = [
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/001-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/002-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/003-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/004-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/005-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/006-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/007-tuya-resized.jpg',
-        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/008-tuya-resized.jpg'
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/001.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/002.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/003.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/004.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/005.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/006.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/007.jpg',
+        'https://raw.githubusercontent.com/ruize-li/colby_digital_studies_storage/master/images/shanghaisketch_48/008.jpg'
       ];
 
 
@@ -49,10 +53,17 @@ const DemoMagazine = () => {
 
 
 const Homepage = () => {
+    
+    function handleDemoClick(e) {
+        window.open("004.pdf", "_blank")
+    }
+
     return(
         <div className="container">
-            <h1>Welcome to Colby Digital Studies!</h1>
-            <button type="button" class="btn btn-info">民族画报</button>
+            <h4>Welcome to Colby Digital Studies!</h4>
+            <h4>Example: 上海漫画第48期</h4>
+            <button type="button" class="btn btn-info" onClick = {handleDemoClick}>示范：搜索</button>
+            <a href='https://www.google.com' target='_blank' rel='noopener noreferrer'>EXAMPLE</a>
             <DemoMagazine />
         </div>
     );
